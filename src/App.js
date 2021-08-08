cdb0295b80a3c985217810405782defd085e1105
sefmadrid <jmadrid147@gmail.com>
2021-08-08 08:09:59 +0800

    update: Added computation for total tip and tip per person.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './sass/App.scss';
 import logo from './images/logo.svg';
@@ -15,10 +15,31 @@ const App = () => {
     const [tip_percent, setTipPercent] = useState('');
     const [number_of_people, setNumberOfPeople] = useState(0);
 
+    const [total_tip, setTotalTip] = useState(0);
+    const [tip_per_person, setTipPerPerson] = useState(0);
+
+    useEffect(() => {
+        setTotalTip(computeTotalTip(bill, isNaN(tip_percent) ? 0 : tip_percent))
+        setTipPerPerson(computeTipPerPerson(total_tip, number_of_people))
+    }, [bill, tip_percent, number_of_people, total_tip])
+
     const reset = () => {
         setBill(0);
         setTipPercent('');
         setNumberOfPeople(0);
+    }
+
+    const computeTotalTip = (bill, tip_percent) => {
+        return ((tip_percent / 100) * bill).toFixed(2)
+    }
+
+    const computeTipPerPerson = (total_tip, number_of_people) => {
+        if(!total_tip || !number_of_people)
+        {
+            return (0).toFixed(2)
+        }
+
+        return (total_tip / number_of_people).toFixed(2)
     }
 
     return (
@@ -31,8 +52,8 @@ const App = () => {
                     <IconInput icon={personIcon} label="Number of People" id="number-of-people" name="number-of-people" placeholder="0" value={number_of_people} onChange={setNumberOfPeople}/>
                 </div>
                 <div id="total">
-                    <TotalSection text="Tip Amount" value="0.00"/>
-                    <TotalSection id="total-section" text="Total" value="0.00"/>
+                    <TotalSection text="Tip Amount" value={total_tip}/>
+                    <TotalSection id="total-section" text="Total" value={tip_per_person}/>
                     <button id="reset-button" onClick={reset}>RESET</button>
                 </div>
             </div>
